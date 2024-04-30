4b310c2f43afb1d4afc5b325c192115a436e276e
vital123725 <153217719+vital123725@users.noreply.github.com>
2024-04-30 22:50:06 +0100

    Add files via upload

---
+++ 4 лаба.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class transport
+{
+public:
+
+    void starting()
+    {
+        cout << "car is starting" << endl;
+    }
+
+    void drive()
+    {
+        cout << "car is moving" << endl;
+    }
+
+    void brake()
+    {
+        cout << "car is braking" << endl;
+    }
+
+    void stalling()
+    {
+        cout << "car is stalling" << endl;
+    }
+};
+
+class Car : public transport
+{
+public:
+    string brand = "toyota";
+    string name = "Camry";
+    string color = "black";
+    int year = 2010;
+
+    void displayInfo()
+    {
+        cout << "brand: " << brand << endl;
+        cout << "name: " << name << endl;
+        cout << "year: " << year << endl;
+        cout << "color: " << color << endl;
+    }
+};
+
+int main()
+{
+    Car car;
+
+    car.drive();
+    car.starting();
+    car.stalling();
+    car.brake();
+
+    car.displayInfo();
+
+    return 0;
+}
